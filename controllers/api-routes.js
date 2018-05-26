5b6615c7067d06cbe774766bbe827ce790c2a9b7
unknown <wchilds9288@gmail.com>
2018-05-26 16:41:54 -0600

    back end

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -3,16 +3,16 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-app.post("/api/posts", function(req, res) {
+app.post("/api/vendor", function(req, res) {
     console.log(req.body);
     db.Vendor.create({
-      first: req.body.first,
-      last: req.body.last,
-      phone: req.body.phone,
+      firstName: req.body.first,
+      lastName: req.body.last,
+      phone_num: req.body.phone,
       address: req.body.address,
       city: req.body.city,
       state: req.body.state,
-      zip: req.body.zip,
+      zip_code: req.body.zip,
       email: req.body.email,
       password: req.body.password
     })
