0178d776990b23a1824725153ac5213caf1e827d
Ken <k3nndroid@gmail.com>
2018-05-28 11:24:01 -0600

    no changes

---
+++ controllers/api-routes.js
@@ -0,0 +1,23 @@
+
+var db = require("../models");
+
+module.exports = function(app) {
+
+app.post("/api/vendor", function(req, res) {
+    console.log(req.body);
+    db.Vendor.create({
+      firstName: req.body.first,
+      lastName: req.body.last,
+      phone_num: req.body.phone,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      zip_code: req.body.zip,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function(dbVendor) {
+        res.json(dbVendor);
+      });
+  });
+};
---
+++ controllers/html-routes.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const passport = require("passport");
+const express = require("express")
+// const router = express.Router()
+
+const isAuthenticated = (request, response, next) => {
+  if ( !request.user ) {
+      // request.flash('You must be logged in for that.')
+      response.redirect('/')
+  } else {
+      return next()
+  }
+}
+const app = express();
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads index.html
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+
+  // signup route loads signup.html
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  })
+
+  // myaccount route loads myaccount.html
+  app.get("/account", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/myaccount.html"));
+  })
+
+  // calendar route loads calendar.html
+  app.get("/calendar", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/calendar.html"));
+  })
+
+  app.post('/login',
+    passport.authenticate('local', {
+      successRedirect: '/account',
+      failureRedirect: '/',
+      // failureFlash: true
+    })
+    // function(req,res){
+    //   console.log("redirect")
+    //   res.redirect("/")
+    // }
+  );
+};
