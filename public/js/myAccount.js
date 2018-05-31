fc28a5f67218853b2efdb16a4fce814bb37b35d9
Eric <ejzagala@gmail.com>
2018-05-30 22:02:04 -0600

    things n stuff

--- public/js/myAccount.js
+++ public/js/myAccount.js
@@ -33,28 +33,60 @@ $(function () {
         }
     })
     
-    // This creates an alert if the fields in the Personal Information area is empty
-    $("#savePiButton").on("click", event => {
-        event.preventDefault(); 
+    $("#yes").on("click", event => {
 
-        var name = $.trim($(".personalInput").val());
-        if (name === "") {
-            alert ("Text field should not be empty");
-            return false;
+        event.preventDefault();
+
+         // Capture vales and build obejct to pass into ajax call
+         const user = {
+            firstName: $("#firstName").val().trim(), 
+            lastName: $("#lastName").val().trim(), 
+            phone: $("#phone").val().trim(),
+            address: $("#address").val().trim(),
+            city: $("#city").val().trim(), 
+            state: $("#state").val().trim(), 
+            zip: $("#zip").val().trim(),
+            email: $("#email").val().trim(), 
+            password: $("#password").val().trim()
         }
-    });
 
-    // This creates an alert if the fields in the "Rate per hour" is empty
-    $("#saveRateButton").on("click", event => {
-        event.preventDefault(); 
+        // Check to see that object was built correctly
+        console.log("The new user is: " + JSON.stringify(user)); 
 
-        var name = $.trim($("#rates").val());
-        if (name === "") {
-            alert ("Text field should not be empty");
-            return false;
-        }
-    });
+        // Make ajax call 
+         $.ajax({
+            method: "PUT",
+            url: "/api/vendor",
+            data: user, 
+        }).then( () => {
+            // Confirm that newUser was posted
+            console.log("Posted new user: " + user)  
+        })
+
+    })
 
   });
 
 
+// Redundant alert code
+// This creates an alert if the fields in the Personal Information area is empty
+    // $("#savePiButton").on("click", event => {
+    //     event.preventDefault(); 
+
+    //     var name = $.trim($(".personalInput").val());
+    //     if (name === "") {
+    //         alert ("Text field should not be empty");
+    //         return false;
+    //     }
+    // });
+
+    // // This creates an alert if the fields in the "Rate per hour" is empty
+    // $("#saveRateButton").on("click", event => {
+    //     event.preventDefault(); 
+
+    //     var name = $.trim($("#rates").val());
+    //     if (name === "") {
+    //         alert ("Text field should not be empty");
+    //         return false;
+    //     }
+    // });
