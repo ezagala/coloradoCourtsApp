8cab9d1f7f5ab088cad81c11efdd723757d0eb8c
unknown <wchilds9288@gmail.com>
2018-06-01 00:09:23 -0600

    google Strat

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -11,7 +11,7 @@ const isAuthenticated = (request, response, next) => {
       return next()
   }
 }
-const app = express();
+// const app = express();
 
 // Routes
 // =============================================================
@@ -39,15 +39,31 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/calendar.html"));
   })
 
-  app.post('/login',
-    passport.authenticate('local', {
-      successRedirect: '/account',
-      failureRedirect: '/',
-      // failureFlash: true
-    })
+
+// passport authentication with google
+app.get('/auth/google',
+passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login',"https://www.googleapis.com/auth/calendar"]}));
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/account');
+  });
+
+  // Local passport authentication
+  // app.post('/login',
+  //   passport.authenticate('local', {
+  //     successRedirect: '/account',
+  //     failureRedirect: '/',
+  //     // failureFlash: true
+  //   })
     // function(req,res){
     //   console.log("redirect")
     //   res.redirect("/")
     // }
-  );
+  // );
 };
--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const session = require("express-session")
 const logger = require("morgan")
-const passport = require('passport'), 
-LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const keys = require("./config/keys")
+// LocalStrategy = require('passport-local').Strategy;
 
 
 const app = express()
-const htmlroutes = require("./controllers/html-routes")
+var PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-var PORT = process.env.PORT || 8080;
+// configure local passport
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"))
+
+// configure logger 
+app.use(logger("dev"))
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -23,22 +33,8 @@ var db = require("./models");
 require("./controllers/api-routes.js")(app);
 require("./controllers/html-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
 
-// configure logger 
-app.use(logger("dev"))
 
-// configure passport
-app.use(session({ secret: 'keyboard cat' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static("public"))
 
 const user = {
     name: "bob",
@@ -46,19 +42,44 @@ const user = {
     id: 1
 }
 
-// defining authentication strategy 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // USER IS A MODEL NEED TO UPDATE
-    
-    if (!username === user.name){
-        return done(null, false, { message: "incorrect username"})
-    } 
-    if (!password === user.password){
-        console.log(password, user.password)
-        return done(null, false, { message: "incorrect password"})
-    }
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+  clientID: keys.google.clientID,
+  clientSecret: keys.google.clientSecret,
+  callbackURL: "/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+    //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //    return done(err, user);
+    //  });
+    console.log(profile)
+
     return done(null, user)
+}
+));
+
+
+
+
+// defining  local authentication strategy 
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     // USER IS A MODEL NEED TO UPDATE
+    
+//     if (!username === user.name){
+//         return done(null, false, { message: "incorrect username"})
+//     } 
+//     if (!password === user.password){
+//         console.log(password, user.password)
+//         return done(null, false, { message: "incorrect password"})
+//     }
+//     return done(null, user)
+    
+    
     // User.findOne({ username: username }, function (err, user) {
     //   if (err) { return done(err); }
     //   if (!user) {
@@ -69,10 +90,9 @@ passport.use(new LocalStrategy(
     //   }
     //   return done(null, user);
     // });
-  }
-));
+//   }
+// ));
 
-app.use("/", htmlroutes)
 passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
@@ -80,10 +100,18 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(id, done) {
     // User.findById(id, function(err, user) {
       done( null, user);
-    //   done( err, user); this is the original
+      // done( err, user); this is the original
     // });
   });
 
-  app.listen(3000)
+  // app.listen(PORT)
 
-  module.exports = app
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+  
+module.exports = app
