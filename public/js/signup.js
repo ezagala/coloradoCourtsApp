409c5ab393c2ebd66fcf2dd313d5f2da9eed1aac
Eric <ejzagala@gmail.com>
2018-06-08 13:16:34 -0600

    Sign up validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
 
-console.log("I'm here");
+    console.log("I'm here");
 
 
     // Listener that hits post route to add new user
@@ -10,30 +10,37 @@ console.log("I'm here");
 
         // Capture vales and build obejct to pass into ajax call
         const user = {
-            firstName: $("#firstName").val().trim(), 
-            lastName: $("#lastName").val().trim(), 
+            firstName: $("#firstName").val().trim(),
+            lastName: $("#lastName").val().trim(),
             phone: $("#phone").val().trim(),
             address: $("#address").val().trim(),
-            city: $("#city").val().trim(), 
-            state: $("#state").val().trim(), 
+            city: $("#city").val().trim(),
+            state: $("#state").val().trim(),
             zip: $("#zip").val().trim(),
-            email: $("#email").val().trim(), 
+            email: $("#email").val().trim(),
             password: $("#password").val().trim()
         }
 
         // Check to see that object was built correctly
-        console.log("The new user is: " + JSON.stringify(user));    
+        console.log("The new user is: " + JSON.stringify(user));
+
+            if (user.firstName === "" || user.lastName === "" || user.email === "" || user.password === "") { 
+                $('#validateModal').modal("show");
+                throw new Error("Required fields are blank!"); 
+            } else {
+                $('#submitModal').modal("show");
+            }
 
         // ajax call to post user
-        $.post('/signup', user, function(user) {
-          console.log('New user', user);
+        $.post('/signup', user, function (user) {
+            console.log('New user', user);
         });
-        
+
     })
 
     // intialize tooltip
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-      })
+    })
 
 });
