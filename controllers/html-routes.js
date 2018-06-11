57132e6957239f9f8082874ea190e1b3893542c5
Eric <ejzagala@gmail.com>
2018-06-11 02:25:45 -0600

    login/sign up

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,15 +1,4 @@
 const path = require("path");
-// const passport = require("passport");
-
-
-const isAuthenticated = (request, response, next) => {
-  if (!request.user) {
-    // request.flash('You must be logged in for that.')
-    response.redirect('/')
-  } else {
-    return next()
-  }
-}
 
 // Routes
 // =============================================================
@@ -27,11 +16,12 @@ module.exports = function (app, passport) {
     passport.authenticate('local-login', {
       successRedirect: '/account',
       failureRedirect: '/',
-    }),
-    function (req, res) {
-      console.log("redirect")
-      res.redirect("/")
-    }
+    })
+    // I don't believe this callback is necessary. Login works w/o it. 
+    // function (req, res) {
+    //   console.log("redirect")
+    //   res.redirect("/")
+    // }
   );
 
   // signup route loads signup.html
@@ -40,24 +30,23 @@ module.exports = function (app, passport) {
   })
 
   // process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/account', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/account', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    // failureFlash : true // allow flash messages
+  })); 
 
   // myaccount route loads myaccount.html
   // isAuthenticated argument removed for testing purposess
-  app.get("/account", function (req, res) {
+  app.get("/account", isLoggedIn, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/myaccount.html"));
   })
 
   // calendar route loads calendar.html
-  app.get("/availability", function (req, res) {
+  app.get("/availability", isLoggedIn, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/availability.html"));
   })
 
-  
 
   // Sign out route 
   app.get('/logout', function (req, res) {
@@ -66,3 +55,24 @@ module.exports = function (app, passport) {
   });
 
 };
+
+// This isn't working
+// function isAuthenticated(request, response, next){
+//   if (!request.user) {
+//     // request.flash('You must be logged in for that.')
+//     response.redirect('/')
+//   } else {
+//     return next() 
+//   }
+// }
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  console.log("The req is", req)
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+      return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
