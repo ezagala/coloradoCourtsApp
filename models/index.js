66de8e0a914a369f70a3898b556b266f1d7790ca
Eric <ejzagala@gmail.com>
2018-07-03 10:57:36 -0600

    Heroku debug on models/index

--- models/index.js
+++ models/index.js
@@ -14,15 +14,15 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//   })
-//   .forEach(file => {
-//     var model = sequelize['import'](path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    var model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 // This was breaking the heroku deploy. It seems to have been meant for the joins that were scrapped. 
 
