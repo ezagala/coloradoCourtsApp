c7312ea2263ac63e7fe336a988529c1bc0d27c01
Eric <ejzagala@gmail.com>
2018-07-03 11:54:58 -0600

    heroku debug

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- -- Drops the ANGEL if it exists currently --
--- DROP DATABASE IF EXISTS ANGEL;
--- -- Creates the "ANGEL" database --
--- CREATE DATABASE ANGEL;
+-- Drops the ANGEL if it exists currently --
+DROP DATABASE IF EXISTS ANGEL;
+-- Creates the "ANGEL" database --
+CREATE DATABASE ANGEL;
